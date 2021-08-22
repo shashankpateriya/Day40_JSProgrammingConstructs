e97d20791cb7a5aae73a10b406dd3df4fa86e7d9
Shashank <shashankpateriya4@gmail.com>
2021-08-22 21:36:45 +0530

    Add project files.

---
+++ SequenceProblems/DiceRoll.js
@@ -0,0 +1,12 @@
+//UC1
+let RandomNumber = Math.floor(Math.random()* 10);
+console.log(RandomNumber);
+
+//UC2
+let DiceRoll = Math.floor(Math.random()* 6) + 1;
+console.log(DiceRoll);
+
+//UC3
+let DiceRoll1 = Math.floor(Math.random()* 6) + 1;
+let DiceRoll2 = Math.floor(Math.random()* 6) + 1;
+console.log(DiceRoll + DiceRoll1);
