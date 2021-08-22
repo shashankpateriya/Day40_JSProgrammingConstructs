3e03c40de91746868ace6c898fa2c453ca8dd0cb
Shashank <shashankpateriya4@gmail.com>
2021-08-22 22:10:16 +0530

    [shashank] Add: Minimum and Maximum of 5 random three digit numbers

---
+++ IfAndElse/5RandomNumMinAndMax.js
@@ -0,0 +1,9 @@
+let firstRandomNum=Math.floor((Math.random() *900)+99);
+let secondRandomNum=Math.floor((Math.random() *900)+99);
+let thirdRandomNum=Math.floor((Math.random() *900)+99);
+let fourthRandomNum=Math.floor((Math.random() *900)+99);
+let fifthRandomNum=Math.floor((Math.random() *900)+99);
+
+
+console.log("Maximum value of 5 random numbers = " + Math.max(firstRandomNum,secondRandomNum,thirdRandomNum,fourthRandomNum,fifthRandomNum));
+console.log("Minimum value of 5 random numbers = " + Math.min(firstRandomNum,secondRandomNum,thirdRandomNum,fourthRandomNum,fifthRandomNum));
