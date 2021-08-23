be0de8f9e7a382fc8edd5a258f2a8e0aaaabab7d
Shashank <shashankpateriya4@gmail.com>
2021-08-23 22:53:49 +0530

    [shashank] Add: Program to check unit place

---
+++ Case/UnitPlace.js
@@ -0,0 +1,29 @@
+let num = 10;
+
+switch(num)
+{
+    case 1:
+        console.log("Units");
+        break;
+    case 10:
+        console.log("Tens");
+        break;
+    case 100:
+        console.log("Hundreds");
+        break;
+    case 1000:
+        console.log("Thousand");
+        break;
+    case 10000:
+        console.log("Tens of Thousands");
+        break;
+    case 100000:
+        console.log("Hundreds of Thousands");
+        break;
+    case 1000000:
+        console.log("Millions ");
+        break;
+    default:
+        console.log("enter correct number");
+        break;
+}
